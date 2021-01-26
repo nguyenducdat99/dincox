22d1435a23335b16db840f8fc3284920efc3d501
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-01-26 23:21:10 +0700

    update 26-01-2021

---
+++ src/components/header/Header.js
@@ -0,0 +1,40 @@
+import './header.scss';
+import logo from './logo-dincox.png';
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="wrapper">
+                <div className="header-grid">
+                    <div className="header-grid__logo">
+                        <h1>
+                            <img src={logo} alt=""/>
+                        </h1>
+                    </div>
+                    <nav className="header-grid__navigation">
+                        <div className="header-grid__navigation__link">
+                            <ul>
+                                <li><a>Sản phẩm</a></li>
+                                <li><a>Nam</a></li>
+                                <li><a>Nữ</a></li>
+                                <li><a>Sale Off</a></li>
+                                <li><a>Hệ thống cửa hàng</a></li>
+                                <li><a>Kiểm tra đơn hàng</a></li>
+                            </ul>
+                        </div>
+                        <div className="header-grid__navigation__search">
+                            <a>Tìm kiếm</a>
+                        </div>
+                        <div className="header-grid__navigation__account">
+                            <a>Tài khoản</a>
+                        </div>
+                        <div className="header-grid__navigation__cart">
+                            <a>Giỏ hàng</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Header;
