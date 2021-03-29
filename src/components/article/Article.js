74f05ba6ce95f92ec44182c4d22dbb06dfb6d573
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-03-29 09:18:23 +0700

    complete responsive for slider 9:18am 29/03/2021

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,22 +1,45 @@
 // import style library
+import { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import './Article.scss';
 
-// code function here
-function Article(props) {
-
-    // setting for slider
-    const settings = {
+function onSettingSlide(slide) {
+    let result = {
         dots: false,
         infinite: true,
         speed: 1000,
-        slidesToShow: 3,
+        slidesToShow: slide,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
         swipe: false
     };
 
+    return {...result}
+}
+
+// code function here
+function Article(props) {
+    //declare state
+    const [presentWidth, setPresentWidth] = useState(window.innerWidth);// store present browser width
+
+    // get present width;
+    useEffect(
+        () => {
+            window.addEventListener('resize', 
+                () => {
+                    setPresentWidth(window.innerWidth);
+                }
+            )
+        },[]
+    )
+
+    // setting for slider
+    var settings = {};
+    if (presentWidth > 768) settings = {...onSettingSlide(3)};
+    if (presentWidth > 480 && presentWidth < 769) settings = {...onSettingSlide(2)};
+    if (presentWidth < 481) settings = {...onSettingSlide(1)};
+    
     // get props
     const { listArticle } = props;
 
