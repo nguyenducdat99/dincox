20f11fc46dc7f23a0dfac2160842f6436cf6fdfe
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-04-09 16:48:51 +0700

    update responsivefor slide article 4:48pm 09/04/2021

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,45 +1,35 @@
 // import style library
-import { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import './Article.scss';
 
-function onSettingSlide(slide) {
-    let result = {
+// code function here
+function Article(props) {
+
+    // setting fo slider
+    const settings = {
         dots: false,
         infinite: true,
         speed: 1000,
-        slidesToShow: slide,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
-        swipe: false
+        swipe: false,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+        ]
     };
-
-    return {...result}
-}
-
-// code function here
-function Article(props) {
-    //declare state
-    const [presentWidth, setPresentWidth] = useState(window.innerWidth);// store present browser width
-
-    // get present width;
-    useEffect(
-        () => {
-            window.addEventListener('resize', 
-                () => {
-                    setPresentWidth(window.innerWidth);
-                }
-            )
-        },[]
-    )
-
-    // setting for slider
-    var settings = {};
-    if (presentWidth > 768) settings = {...onSettingSlide(3)};
-    if (presentWidth > 480 && presentWidth < 769) settings = {...onSettingSlide(2)};
-    if (presentWidth < 481) settings = {...onSettingSlide(1)};
-    
     // get props
     const { listArticle } = props;
 
