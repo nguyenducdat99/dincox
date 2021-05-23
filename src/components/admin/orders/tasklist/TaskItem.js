88cf215ac4b8f02c1a8d16f9c691143ecdae3765
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-05-23 21:54:10 +0700

    update order logic

--- src/components/admin/orders/tasklist/TaskItem.js
+++ src/components/admin/orders/tasklist/TaskItem.js
@@ -1,192 +1,182 @@
 import { useEffect, useState } from "react";
 import { mapValueAndName } from "../../../../constants/GlobalVariables";
 
-
-
 function TaskItem(props) {
+  //  get props, declare variable, constant
+  const { index, itemRec, onUpdateStatusRec, openDetail, handlePrint } = props;
 
+  // declare state component;
+  const [statusItem, setStatusItem] = useState(-1);
 
-    //  get props, declare variable, constant
-    const { 
-        index, 
-        itemRec,
-        onUpdateStatusRec,
-        openDetail,
-        handlePrint 
-    } = props;
+  // load status from data
+  useEffect(() => {
+    setStatusItem(itemRec.status);
 
-    // declare state component;
-    const [statusItem, setStatusItem] = useState(-1)
+    // eslint-disable-next-line
+  }, []);
 
-    // load status from data
-    useEffect(
-        () => {
-            setStatusItem(itemRec.status);
+  // handle when change status
+  const onHandleChange = (event) => {
+    const value = event.target.value;
 
-            // eslint-disable-next-line
-        },[]
-    )
+    const confirm = window.confirm("Bạn muốn thay đổi trạng thái đơn hàng?");
 
-    // handle when change status
-    const onHandleChange = event => {
-        const value = event.target.value;
-
-        const confirm = window.confirm('Bạn muốn thay đổi trạng thái đơn hàng?');
-
-        if (confirm) {
-            onUpdateStatusRec(
-                {
-                    id_order: itemRec.id_order,
-                    status: value*1
-                }
-            )
-            setStatusItem(value);
-        } 
+    if (confirm) {
+      onUpdateStatusRec({
+        id_order: itemRec.id_order,
+        status: value * 1,
+      });
+      setStatusItem(value);
     }
+  };
 
-    // handle return item for onSlectItem
-    const onSelectItem = () => {
-        openDetail(itemRec);
+  // handle return item for onSlectItem
+  const onSelectItem = () => {
+    openDetail(itemRec);
+  };
+
+  // return option for status of order
+  const optionStatus = () => {
+    switch (statusItem * 1) {
+      case -1:
+        return (
+          <option value={mapValueAndName[4].value}>
+            {mapValueAndName[4].name}
+          </option>
+        );
+      case 0:
+        return (
+          <>
+            <option value={mapValueAndName[4].value}>
+              {mapValueAndName[4].name}
+            </option>
+            <option value={mapValueAndName[0].value}>
+              {mapValueAndName[0].name}
+            </option>
+            <option value={mapValueAndName[1].value}>
+              {mapValueAndName[1].name}
+            </option>
+            <option value={mapValueAndName[2].value}>
+              {mapValueAndName[2].name}
+            </option>
+            <option value={mapValueAndName[3].value}>
+              {mapValueAndName[3].name}
+            </option>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <option value={mapValueAndName[1].value}>
+              {mapValueAndName[1].name}
+            </option>
+            <option value={mapValueAndName[2].value}>
+              {mapValueAndName[2].name}
+            </option>
+            <option value={mapValueAndName[3].value}>
+              {mapValueAndName[3].name}
+            </option>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <option value={mapValueAndName[2].value}>
+              {mapValueAndName[2].name}
+            </option>
+            <option value={mapValueAndName[3].value}>
+              {mapValueAndName[3].name}
+            </option>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <option value={mapValueAndName[3].value}>
+              {mapValueAndName[3].name}
+            </option>
+          </>
+        );
+      default:
+        return mapValueAndName.map((element, index) => {
+          return (
+            <option key={index} value={element.value}>
+              {element.name}
+            </option>
+          );
+        });
     }
-    
+  };
 
-    // return option for status of order
-    const optionStatus = () => {
+  var colorSelect = "";
+  switch (statusItem * 1) {
+    case -1:
+      colorSelect = "huy";
+      break;
+    case 0:
+      colorSelect = "dat-hang";
+      break;
+    case 1:
+      colorSelect = "xac-nhan";
+      break;
+    case 2:
+      colorSelect = "giao-hang";
+      break;
+    case 3:
+      colorSelect = "thanh-cong";
+      break;
 
-        switch (statusItem*1) {
-            case -1:
-                return (
-                    <option value={mapValueAndName[4].value}> 
-                        {mapValueAndName[4].name}
-                    </option>
-                )
-            case 0:
-                return (
-                    <>
-                        <option value={mapValueAndName[0].value}> 
-                            {mapValueAndName[0].name}
-                        </option>
-                        <option value={mapValueAndName[1].value}> 
-                            {mapValueAndName[1].name}
-                        </option>
-                        <option value={mapValueAndName[2].value}> 
-                            {mapValueAndName[2].name}
-                        </option>
-                        <option value={mapValueAndName[3].value}> 
-                            {mapValueAndName[3].name}
-                        </option>
-                    </>
-                )
-            case 1:
-                return (
-                    <>
-                        <option value={mapValueAndName[1].value}> 
-                            {mapValueAndName[1].name}
-                        </option>
-                        <option value={mapValueAndName[2].value}> 
-                            {mapValueAndName[2].name}
-                        </option>
-                        <option value={mapValueAndName[3].value}> 
-                            {mapValueAndName[3].name}
-                        </option>
-                    </>
-                )
-            case 2:
-                return (
-                    <>
-                        <option value={mapValueAndName[2].value}> 
-                            {mapValueAndName[2].name}
-                        </option>
-                        <option value={mapValueAndName[3].value}> 
-                            {mapValueAndName[3].name}
-                        </option>
-                    </>
-                )
-            case 3:
-                return (
-                    <>
-                        <option value={mapValueAndName[3].value}> 
-                            {mapValueAndName[3].name}
-                        </option>
-                    </>
-                )
-            default:
-                return (
-                    mapValueAndName.map(
-                        (element,index) => {
-                            return (
-                                <option key={index} value={element.value}> 
-                                    {element.name}
-                                </option>
-                            )
-                        }
-                    )
-                )
-        }
-    }
+    default:
+      break;
+  }
 
-    var colorSelect = '';
-    switch (statusItem*1) {
-        case -1:
-            colorSelect = 'huy';
-            break;
-        case 0:
-            colorSelect = 'dat-hang';
-            break;
-        case 1:
-            colorSelect = 'xac-nhan';
-            break;
-        case 2:
-            colorSelect = 'giao-hang';
-            break;
-        case 3:
-            colorSelect = 'thanh-cong';
-            break;
-    
-        default:
+  const onHanlePrint = () => {
+    openDetail(itemRec, true);
 
-            break;
-    }
+    setTimeout(() => {
+      handlePrint();
+    }, 500);
+  };
 
-    const onHanlePrint = () => {
-        openDetail(itemRec,true);
-        
-        setTimeout(()=>{
-            handlePrint();
-        },500)
-    }
-
-    // return ui
-    return (
-        <tr className={index%2===0?"task-list__table__line-odd":''}>
-            <td>{index}</td>
-            <td>{'dincox'+itemRec.id_order}</td>
-            <td>{itemRec.receiver}</td>
-            <td>{itemRec.sent_to}</td>
-            <td>
-                <p>{itemRec.number_phone}</p>
-                <p>{itemRec.email}</p>
-            </td>
-            <td>{itemRec.create_at}</td>
-            <td>
-                <select
-                    value={statusItem}
-                    onChange={onHandleChange}
-                    className={colorSelect}
-                >
-                    {optionStatus()}
-                </select>
-            </td>
-            <td>
-                <button type="button" className="btn btn-warning" onClick={onSelectItem}>
-                    <span className="fa fa-search"></span>Chi tiết
-                </button>
-                <button type="button" className="btn btn-warning" onClick={onHanlePrint}>
-                    <span className="fa fa-print"></span>In hóa đơn
-                </button>
-            </td>
-        </tr>
-    )
+  // return ui
+  return (
+    <tr className={index % 2 === 0 ? "task-list__table__line-odd" : ""}>
+      <td>{index}</td>
+      <td>{"dincox" + itemRec.id_order}</td>
+      <td>{itemRec.receiver}</td>
+      <td>{itemRec.sent_to}</td>
+      <td>
+        <p>{itemRec.number_phone}</p>
+        <p>{itemRec.email}</p>
+      </td>
+      <td>{itemRec.create_at}</td>
+      <td>
+        <select
+          value={statusItem}
+          onChange={onHandleChange}
+          className={colorSelect}
+        >
+          {optionStatus()}
+        </select>
+      </td>
+      <td>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={onSelectItem}
+        >
+          <span className="fa fa-search"></span>Chi tiết
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={onHanlePrint}
+        >
+          <span className="fa fa-print"></span>In hóa đơn
+        </button>
+      </td>
+    </tr>
+  );
 }
 
 export default TaskItem;
--- src/components/product/productcollection/ProductCollection.js
+++ src/components/product/productcollection/ProductCollection.js
@@ -1,59 +1,81 @@
 // import style library
-import './ProductCollection.scss';
-import * as Actions from '../../../constants/Config';
+import "./ProductCollection.scss";
+import * as Actions from "../../../constants/Config";
+import ImageZoom from "react-medium-image-zoom";
 
 function makeRandomIndex(max) {
-    return Math.floor((Math.random() * max))
+  return Math.floor(Math.random() * max);
 }
 
 function makeRandomPath(items) {
-    let result = '';
+  let result = "";
 
-    
-    if (items.length === 0) {
-        return result;
-    }else{
-        result = result + Actions.API_URL + (items[makeRandomIndex(items.length)].path);
-    }
-
+  if (items.length === 0) {
     return result;
+  } else {
+    result =
+      result + Actions.API_URL + items[makeRandomIndex(items.length)].path;
+  }
+
+  return result;
 }
 
 // code function here
-function ProductCollection(props){
-    // get props 
-    const {imagesRec} = props;
+function ProductCollection(props) {
+  // get props
+  const { imagesRec } = props;
 
-    // return list small image
-    var listItem = [0,1,2,3,4,5].map(
-        (element, index) => {
-            return <div key={index} className="product__collection__list-small__single">
-                        <img 
-                            src={makeRandomPath(imagesRec)} 
-                            alt="dincox collection" 
-                        />
-                    </div>
-        }
-    )
+  // return list small image
+  const listItem = [0, 1, 2, 3, 4, 5].map((element, index) => {
+    const path = makeRandomPath(imagesRec);
 
-    return(
-        <div className="product__collection">
-                    <h2>Kho ảnh & video</h2>
-                    <div className="product__collection-grid">
-                        <div className="product__collection__list-small">
-                            {
-                                listItem
-                            }
-                        </div>
-                        <div className="product__collection__big">
-                            <img 
-                                src={makeRandomPath(imagesRec)} 
-                                alt="dincox collection" 
-                            />
-                        </div>
-                    </div>
-                </div>
-               
+    return (
+      <div key={index} className="product__collection__list-small__single">
+        {/* <img src={makeRandomPath(imagesRec)} alt="dincox collection" /> */}
+        <ImageZoom
+          image={{
+            src: path,
+            alt: "dincox collection" + element,
+            style: {
+              width: "100%",
+              height: "100%",
+            },
+          }}
+          zoomImage={{
+            src: path,
+            alt: "dincox collection" + element,
+          }}
+        />
+      </div>
     );
+  });
+
+  const pathMain = makeRandomPath(imagesRec);
+
+  return (
+    <div className="product__collection">
+      <h2>Kho ảnh & video</h2>
+      <div className="product__collection-grid">
+        <div className="product__collection__list-small">{listItem}</div>
+        <div className="product__collection__big">
+          {/* <img src={makeRandomPath(imagesRec)} alt="dincox collection" /> */}
+          <ImageZoom
+            image={{
+              src: pathMain,
+              alt: "dincox collection",
+              style: {
+                width: "100%",
+                height: "100%",
+              },
+            }}
+            zoomImage={{
+              src: pathMain,
+              alt: "dincox collection",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ProductCollection;
